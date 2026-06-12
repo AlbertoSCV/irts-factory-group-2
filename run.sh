958533072c7118b27dae9b12e1401bab23350648
Alberto Somoza <asomoza@silverpeak.tech>
2026-06-12 20:42:10 +0000

    Initial Commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$DIR/cartago-Factory"
+
+# Using the machine's system Java instead of the local runtime
+java -Djava.awt.headless=false -cp "$DIR/jason/jason-interpreter-3.3.2.jar:$DIR/jason/jade-4.3.jar:$DIR/jason/javax.json-api-1.1.4.jar:$DIR/jason/javax.json-1.1.4.jar:$DIR/cartago-Factory/lib/*:$DIR/cartago-Factory/bin" jason.infra.local.RunLocalMAS factory1.mas2j
